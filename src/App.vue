e53749470e0d4167ce57fe41e6b4a5c794fb228e
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-03 18:01:29 +0800

    https://tailwindcss.com/docs/guides/vite

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
